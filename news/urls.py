e829be564159bedffa83d8599306a64b6ef949ca
comey <comey@219.216.78.18>
2016-09-12 12:38:58 +0800

    1.convert function based views to class based views. 2.add view of newslist without media.

--- news/urls.py
+++ news/urls.py
@@ -4,10 +4,11 @@ import news.views
 
 urlpatterns = [
     url(r'^$', news.views.news_home, name='news_home'),
-    url(r'^(?P<source>\w+)/$', news.views.news_list, name='story_list'),
-    url(r'^detail/(?P<story_id>\d+)/$', news.views.story_detail, name="story_detail"),
-    url(r'^convertlist/(?P<source>\w+)/$', news.views.convert_list, name='convert_list'),
-    url(r'^convertdetail/(?P<source>\w+)/(?P<id>\d+)/$', news.views.convert_detail, name='convert_detail'),
     url(r'^about/$', news.views.about, name='about'),
+    url(r'^(?P<source>\w+)/$', news.views.NewsList.as_view(), name='story_list'),
+    url(r'^detail/(?P<story_id>\d+)/$', news.views.StoryDetail.as_view(), name="story_detail"),
+    url(r'^convertlist/(?P<source>\w+)/$', news.views.ConvertList.as_view(), name='convert_list'),
+    url(r'^convertdetail/(?P<source>\w+)/(?P<id>\d+)/$', news.views.ConvertDetail.as_view(), name='convert_detail'),
+    
 ]
 
--- news/views.py
+++ news/views.py
@@ -10,137 +10,162 @@ from django.shortcuts import render_to_response
 from django.http.response import HttpResponse, HttpResponseRedirect
 from django.views.decorators.gzip import gzip_page
 from django.core.urlresolvers import reverse
+from django.views.generic.base import View, TemplateView
+from django.utils.decorators import method_decorator
+from django.template.context import RequestContext
 
 from news.models import Story, DailyDate
 from news.fetcher import CBFetcher, PageNotFoundError
-
-
-def picture_display(request):
-    image_show = request.GET.get("image", None)
-    if image_show:
-        request.session['image'] = image_show
-    else:
-        image_show = request.session.get('image', 'hide')
-    if image_show == 'show':
-        return True
-    else:
-        return False
 
 
 def news_home(request):
     return HttpResponseRedirect(reverse('news:story_list', kwargs={'source': 'zhihudaily'}))
 
 
-@gzip_page
-def news_list(request, source):
-    date_str = request.GET.get("date", None)
-    if date_str:
-        date = datetime.datetime.strptime(date_str, '%Y-%m-%d').date()
-    else:
-        date = datetime.datetime.now().date()
+class NewsViewBase(TemplateView):
 
-    date_str = date.strftime('%Y-%m-%d')
-    dailydate = DailyDate.objects.update_daily_date_with_date(
-        date, source=source)
-    story_qs = dailydate.get_daily_stories()
-    last_date = date - datetime.timedelta(days=1)
-    last_date_str = last_date.strftime('%Y-%m-%d')
-    next_date = date + datetime.timedelta(days=1)
-    next_date_str = next_date.strftime('%Y-%m-%d')
-    if date == datetime.datetime.now().date():
-        next_date_str = ''
-    context = {}
-    context['date_str'] = date_str
-    context['story_qs'] = story_qs
-    context['last_date_str'] = last_date_str
-    context['next_date_str'] = next_date_str
-    context['nav_item'] = 'zhihu'
-    return render_to_response('newslist.html', context)
+    @method_decorator(gzip_page)
+    def get(self, request, *args, **kwargs):
+        return TemplateView.get(self, request, *args, **kwargs)
 
+    def media_display(self):
+        image_show = self.request.GET.get("image", None)
+        if image_show:
+            self.request.session['image'] = image_show
+        else:
+            image_show = self.request.session.get('image', 'hide')
+        if image_show == 'show':
+            return True
+        else:
+            return False
 
-@gzip_page
-def story_detail(request, story_id):
-    story = Story.objects.get(story_id=story_id)
-    story.update()
+    def get_date(self):
+        date_str = self.request.GET.get("date", None)
+        if date_str:
+            date = datetime.datetime.strptime(date_str, '%Y-%m-%d').date()
+        else:
+            date = datetime.datetime.now().date()
+        date_str = date.strftime('%Y-%m-%d')
+        last_date = date - datetime.timedelta(days=1)
+        last_date_str = last_date.strftime('%Y-%m-%d')
+        next_date = date + datetime.timedelta(days=1)
+        next_date_str = next_date.strftime('%Y-%m-%d')
+        if date == datetime.datetime.now().date():
+            next_date_str = ''
+        return date, last_date_str, date_str, next_date_str
 
-    image_show = picture_display(request)
-    if not image_show:
-        story.body = hide_media(story.body)
-    return render_to_response('story_detail.html', {'story': story, 'nav_item': 'zhihu'})
+    def hide_media(self, raw_html):
+        soup = BeautifulSoup(raw_html, "html.parser")
+        for img in soup.find_all('img'):
+            tag = soup.new_tag("a")
+            tag['href'] = img['src']
+            tag.string = img.get('title', '图片')
+            img.insert_before(tag)
+            img.decompose()
 
+        for embed in soup.find_all('embed'):
+            tag = soup.new_tag("a")
+            tag['href'] = embed['src']
+            tag.string = '视频' + embed['src']
+            embed.insert_before(tag)
+            embed.decompose()
 
-@gzip_page
-def convert_list(request, source):
-    page_number = int(request.GET.get('page', 1))
-    if source == 'cb':
-        result = CBFetcher().get_news_list(page_number)
-        news_list = result['news_list']
+        for script in soup.find_all('script'):
+            script.decompose()
+
+        return str(soup)
+
+    def get_context_data(self, request, *args, **kwargs):
         context = {}
-        context['page_number'] = page_number
-        if page_number > 1:
-            context['previous_page'] = page_number - 1
-        context['next_page'] = page_number + 1
+        return context
 
-        context['news_list'] = news_list
+
+class NewsList(NewsViewBase):
+    template_name = 'newslist.html'
+
+    def get_context_data(self, source, **kwargs):
+        if not self.media_display():
+            self.template_name = 'newslist_no_pic.html'
+        
+        date, last_date_str, date_str, next_date_str = self.get_date()
+        dailydate = DailyDate.objects.update_daily_date_with_date(
+            date, source=source)
+        story_qs = dailydate.get_daily_stories()
+        context = {}
+        context['date_str'] = date_str
+        context['story_qs'] = story_qs
+        context['last_date_str'] = last_date_str
+        context['next_date_str'] = next_date_str
+        context['nav_item'] = 'zhihu'
+        return context
+
+
+class StoryDetail(NewsViewBase):
+    template_name = 'story_detail.html'
+
+    def get_context_data(self, story_id, **kwargs):
+        story = Story.objects.get(story_id=story_id)
+        story.update()
+
+        if not self.media_display():
+            story.body = self.hide_media(story.body)
+        context = {}
+        context['story'] = story
+        context['nav_item'] = 'zhihu'
+        return context
+
+
+class ConvertList(NewsViewBase):
+    template_name = 'convert_list.html'
+
+    def get_context_data(self, source, **kwargs):
+        page_number = int(self.request.GET.get('page', 1))
+        if source == 'cb':
+            result = CBFetcher().get_news_list(page_number)
+            news_list = result['news_list']
+            context = {}
+            context['page_number'] = page_number
+            if page_number > 1:
+                context['previous_page'] = page_number - 1
+            context['next_page'] = page_number + 1
+    
+            context['news_list'] = news_list
+            context['nav_item'] = 'cnbeta'
+            return context
+        else:
+            raise Exception('wrong source name')
+
+
+class ConvertDetail(NewsViewBase):
+    template_name = 'convert_detail.html'
+
+    def get_context_data(self, source, id, **kwargs):
+        try:
+            result = CBFetcher().get_story_detail(id, update_comment=True)
+        except PageNotFoundError:
+            raise Exception('page not found')
+        body = str(result['body'])
+        if not self.media_display():
+            body = self.hide_media(body)
+
+        context = {}
+        context['body'] = body
+        context['share_url'] = result['share_url']
+        context['section_name'] = result['section']['name']
+        context['title'] = result['title']
+        context['author'] = result['author']
+        context['time'] = result['time']
+        context['summary'] = str(result['summary'])
+        context['comments'] = str(result['comments'])
+        context['comment_count_all'] = result['comment_count_all']
+        context['comment_count_show'] = result['comment_count_show']
         context['nav_item'] = 'cnbeta'
-        return render_to_response('convert_list.html', context)
-    else:
-        return HttpResponse('wrong source name')
-
-
-@gzip_page
-def convert_detail(request, source, id):
-
-    try:
-        result = CBFetcher().get_story_detail(id, update_comment=True)
-    except PageNotFoundError:
-        return HttpResponse('page not found')
-    soup = BeautifulSoup()
-    body = str(result['body'])
-    image_show = picture_display(request)
-    if not image_show:
-        body = hide_media(body)
-
-
-    context = {}
-    context['body'] = body
-    context['share_url'] = result['share_url']
-    context['section_name'] = result['section']['name']
-    context['title'] = result['title']
-    context['author'] = result['author']
-    context['time'] = result['time']
-    context['summary'] = str(result['summary'])
-    context['comments'] = str(result['comments'])
-    context['comment_count_all'] = result['comment_count_all']
-    context['comment_count_show'] = result['comment_count_show']
-    context['nav_item'] = 'cnbeta'
-    return render_to_response('convert_detail.html', context)
-
+        return context
 
 @gzip_page
 def about(request):
     return render_to_response('about.html', {'nav_item': 'about'})
 
 
-def hide_media(raw_html):
-    soup = BeautifulSoup(raw_html, "html.parser")
 
-    for img in soup.find_all('img'):
-        tag = soup.new_tag("a")
-        tag['href'] = img['src']
-        tag.string = img.get('title', '图片')
-        img.insert_before(tag)
-        img.decompose()
-
-    for embed in soup.find_all('embed'):
-        tag = soup.new_tag("a")
-        tag['href'] = embed['src']
-        tag.string = '视频' + embed['src']
-        embed.insert_before(tag)
-        embed.decompose()
-
-    for script in soup.find_all('script'):
-        script.decompose()
-
-    return str(soup)
 
